c7f0cc49c07e3a5fd105cf296fa7c8b547b6c27f
swoorr <furkansahin23@gmail.com>
2021-01-03 01:45:56 +0300

    first commit

---
+++ src/lib/Banners.js
@@ -0,0 +1,11 @@
+const Banners = () => {
+  return (
+    <div className="container-fluid banners">
+        <div className="row">
+        <img src="https://i.imgur.com/BUvexK1.png"/>
+        </div>
+    </div>
+  );
+};
+
+export default Banners;
---
+++ src/lib/Category.js
@@ -0,0 +1,12 @@
+const Category = () => {
+  return (
+    <div className="container-fluid Category">
+        <div className="row">
+        <img src="https://i.imgur.com/BUvexK1.png"/>
+        </div>
+        <h2></h2>
+    </div>
+  );
+};
+
+export default Category;
